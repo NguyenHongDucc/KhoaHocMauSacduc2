c05ca279e54fe72528cdfc2ad0f68da1ed24ef8e
NguyenHongDucc <hongduc9278@gmail.com>
2025-05-10 20:43:56 +0700

    Update Khoa học Màu sắc.py

--- Khoa học Màu sắc.py
+++ Khoa học Màu sắc.py
@@ -12,7 +12,7 @@ st.write("# Khoa in và truyền thông")
 st.markdown(
     """
     <div style="display: flex; justify-content: flex-end;">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0pmrFxarCWzA1Pff4qQQrIvEyPei9oN3wog&s" width="130" style="margin-right:10px;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Logo_Tr%C6%B0%E1%BB%9Dng_%C4%90%E1%BA%A1i_H%E1%BB%8Dc_S%C6%B0_Ph%E1%BA%A1m_K%E1%BB%B9_Thu%E1%BA%ADt_TP_H%E1%BB%93_Ch%C3%AD_Minh.png" width="130" style="margin-right:10px;">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ25mw5Ic55r3Y83GvxAB0h-63315IooQNaA&s" width="130">
     </div>
     """,
