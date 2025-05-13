532d5d032ed3d25b9df2535ec3b2649f493f09d6
NguyenHongDucc <hongduc9278@gmail.com>
2025-05-13 20:49:28 +0700

    Update Khoa học Màu sắc.py

--- Khoa học Màu sắc.py
+++ Khoa học Màu sắc.py
@@ -5,3 +5,16 @@ st.set_page_config(
 
 st.write("### Chào mừng bạn đến với project Khoa học Màu sắc của mình!")
 st.write("# Mình tên là Nguyễn Hồng Đức, MSSV 22158053")
+st.write("# Mình tên là Vũ Mạnh Tuấn, MSSV 22158106")
+st.write("# Mình tên là Đoàn Lê Việt Phát, MSSV 22158077")
+
+
+st.markdown(
+    """
+    <div style="display: flex; justify-content: flex-end;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0pmrFxarCWzA1Pff4qQQrIvEyPei9oN3wog&s" width="150" style="margin-right:10px;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ25mw5Ic55r3Y83GvxAB0h-63315IooQNaA&s" width="150">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
